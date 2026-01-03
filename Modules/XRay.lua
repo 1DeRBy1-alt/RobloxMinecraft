704234fd08ad4bdd9f65a5d16ef4cb4d9240679a
1DeRBy1 <paiderby9.54@gmail.com>
2026-01-03 22:16:19 +0200

    No logic yet

---
+++ Modules/XRay.lua
@@ -0,0 +1,7 @@
+  -- Services --
+local Players = game:GetService("Players")
+
+  -- Variables --
+local player = Players.LocalPlayer
+
+local WorldFunctions = loadstring(game:HttpGet("https://raw.githubusercontent.com/1DeRBy1-alt/RobloxMinecraft/main/WorldTestFunctions.lua"))()
