47c2b01d9c3207e44c8480cb1ded3dca58e6b70f
1DeRBy1 <paiderby9.54@gmail.com>
2026-01-03 22:23:04 +0200

    added get player position

--- Modules/XRay.lua
+++ Modules/XRay.lua
@@ -1,7 +1,18 @@
-  -- Services --
+ -- Services --
 local Players = game:GetService("Players")
 
-  -- Variables --
+ -- Variables --
 local player = Players.LocalPlayer
+local Character = player.Character or player.CharacterAdded:Wait()
+local hrp = Character:FindFirstChild("HumanoidRootPart")
+local pos = hrp.Position
 
+ -- World Functions --
 local WorldFunctions = loadstring(game:HttpGet("https://raw.githubusercontent.com/1DeRBy1-alt/RobloxMinecraft/main/WorldTestFunctions.lua"))()
+
+ -- Functions --
+function getPosition()
+    if WorldFunctions and WorldFunctions.WorldToBlock then
+        return WorldFunctions.WorldToBlock(pos.X, pos.Y, pos.Z)
+    end
+end
