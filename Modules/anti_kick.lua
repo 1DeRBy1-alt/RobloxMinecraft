c01a6860d3cd2fe0fca29fb6ec0dbc63a68c1191
1DeRBy1 <paiderby9.54@gmail.com>
2026-01-10 22:28:52 +0200

    Update and rename adonisbypass.lua to anti_kick.lua

--- Modules/anti_kick.lua
+++ Modules/anti_kick.lua
@@ -9,3 +9,20 @@ for _, v in next, filtergc("table", {Keys = {"RLocked", "Detected"}}, true) or {
     if type(v) ~= "function" or isfunctionhooked(v) then continue end
     hookfunction(v, newcclosure(function() coroutine.yield() end))
 end
+task.wait()
+
+local oldhmmi
+local oldhmmnc
+oldhmmi = hookmetamethod(game, "__index", function(self, method)
+    if self == player and method:lower() == "kick" then
+        return error("Expected ':' not '.' calling member function Kick", 2)
+    end
+    return oldhmmi(self, method)
+end)
+
+oldhmmnc = hookmetamethod(game, "__namecall", function(self, ...)
+    if self == player and getnamecallmethod():lower() == "kick" then
+        return
+    end
+    return oldhmmnc(self, ...)
+end)
