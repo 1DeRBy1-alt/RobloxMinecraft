d141320ef779c209df3d80d383845eba4d4cbac8
1DeRBy1 <paiderby9.54@gmail.com>
2026-01-13 06:34:20 +0200

    Update globals.lua

--- Modules/globals.lua
+++ Modules/globals.lua
@@ -1,10 +1,12 @@
  -- Toggles --
 _G.kaConn = false
 _G.mobKillaura = false
+_G.scaffoldEnabled = false
 _G.hitboxConn = false
 _G.xrayConn = false
 _G.BlackCover = false
  -- Configuration --
 _G.kaDelay = 0.02
+_G.scaffoldDelay = 0.01
 _G.hitboxSize = 9
 _G.kaTeamCheck = true
